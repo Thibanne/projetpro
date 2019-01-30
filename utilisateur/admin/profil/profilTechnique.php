d4140d0d85a6ea8e8435c8f60265036735630a25
Thibanne <thibault_anne@orange.fr>
2019-01-30 11:58:39 +0100

    Finition des sql et tableaux de Profil

--- utilisateur/admin/profil/profilTechnique.php
+++ utilisateur/admin/profil/profilTechnique.php
@@ -1,36 +1,82 @@
 <?php
 $connect = con();
 if(isset($_GET['id'])){
+  // Degats
   $sql = "SELECT
-    Technique.id AS 'idTech',
-    Technique.Nom AS 'techNom',
-    CoutTechnique.Valeur AS 'valCout',
-    DegatTechnique.Valeur AS 'valDegat',
-    Stats.Nom AS 'nomStats'
+  Technique.id AS 'idTech',
+  Technique.Nom AS 'techNom',
+  DegatTechnique.Valeur AS 'valDegat',
+  statsDegat.Nom AS 'nomDegat'
   FROM Technique
-  LEFT JOIN CoutTechnique ON Technique.id = CoutTechnique.id_technique
   LEFT JOIN DegatTechnique ON Technique.id = DegatTechnique.id_technique
-  LEFT JOIN Stats ON Technique.id = Stats.id
+  LEFT JOIN Stats AS `statsDegat` ON DegatTechnique.id_stats = statsDegat.id
   WHERE Technique.id = $_GET[id]";
   $result = $connect->query($sql);
-  $tableTechProfil = $result->fetch_all(MYSQLI_ASSOC);
-?>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th>Valeur de <?= $tableTechProfil['0']['techNom'] ?></th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach($tableTechProfil as $key => $value){ ?>
-    <tr>
-      <th><?= $value['valCout'] ?></th>
-      <th><?= $value['nomStats'] ?></th>
-      <th><?= $value['valDegat'] ?></th>
-      <th><?= $value['nomStats'] ?></th>
-    </tr>
+  $tableTechProfilDegat = $result->fetch_all(MYSQLI_ASSOC);
+  // Cout
+  $sql = "SELECT
+  Technique.id AS 'idTech',
+  Technique.Nom AS 'techNom',
+  CoutTechnique.Valeur AS 'valCout',
+  statsCout.Nom AS 'nomCout'
+  FROM Technique
+  LEFT JOIN CoutTechnique ON Technique.id = CoutTechnique.id_technique
+  LEFT JOIN Stats AS `statsCout` ON CoutTechnique.id_stats = statsCout.id
+  WHERE Technique.id = $_GET[id]";
+  $result = $connect->query($sql);
+  $tableTechProfilCout = $result->fetch_all(MYSQLI_ASSOC);
+  ?>
+  <div class="TableTechCentralAlign">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col" style="width:33vw;">Valeur de <?= $tableTechProfilDegat['0']['techNom'] ?></th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="col">Dégât</th>
+          <th scope="col">Coût</th>
+        </tr>
+      </tbody>
+    </table>
+    <div class="tableTechProfilMiddleReduc">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Valeur</th>
+            <th scope="col">Stats</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach($tableTechProfilDegat as $key => $value){ ?>
+            <tr>
+              <th scope="row"><?= $value['valDegat'] ?></th>
+              <th><?= $value['nomDegat'] ?></th>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </div>
+    <div class="tableTechProfilMiddleReduc">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Valeur</th>
+            <th scope="col">Stats</th>
+          </tr>
+        </thead>
+        <tbody>
+            <?php foreach($tableTechProfilCout as $key => $value){ ?>
+            <tr>
+              <th scope="row"><?= $value['valCout'] ?></th>
+              <th><?= $value['nomCout'] ?></th>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </div>
   <?php } ?>
-  </tbody>
-</table>
-<?php } ?>
-<a href="/?page=liste-technique" class="btn btn-secondary">Retour liste</a>
+  <a href="/?page=liste-technique" class="btn btn-secondary">Retour liste</a>
+</div>
